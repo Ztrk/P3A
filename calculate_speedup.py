94953bb807d73fa58c195de00bc4e3c4f8ae9451
Raffel222 <Raffel222@gmail.com>
2020-12-16 00:36:19 +0100

    Calculates the acceleration of algorithm execution for particular number of instances
    argv[1] - file name with the algorithm execution time for more processors, argv[1] - file name with the algorithm execution time for one processor

---
+++ calculate_speedup.py
@@ -0,0 +1,16 @@
+import sys
+
+#argv[1] - 10 proc , argv[2] - 1 proc
+with open(sys.argv[1]) as proc_10:
+	content_10 = proc_10.readlines()
+with open(sys.argv[2]) as proc_1:
+	content_1 = proc_1.readlines()
+speedups = open("different_inst_speedups.txt", "w")
+speedups.write(content_10[0].split()[0] + " speedup:\n")
+content_10.pop(0)
+content_1.pop(0)
+for t10,t1 in zip (content_10, content_1):
+	t10l = t10.split()
+	t1l = t1.split()
+	speedups.write( str(t10l[0]) + " " + str( float(t1l[1]) / float(t10l[1]) ) + "\n")
+speedups.close()
