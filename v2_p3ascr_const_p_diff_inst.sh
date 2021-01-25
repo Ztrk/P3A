9ad4e8b2b62cda8e5fb434b4f5160aec21710885
Raffel222 <Raffel222@gmail.com>
2021-01-25 11:57:24 +0100

    Update v2_p3ascr_const_p_diff_inst.sh

--- v2_p3ascr_const_p_diff_inst.sh
+++ v2_p3ascr_const_p_diff_inst.sh
@@ -2,29 +2,26 @@
 #SBATCH --job-name=p3arun
 #SBATCH --output=p3arun.out
 #SBATCH --error=p3arun.err
-#SBATCH --time=00:02:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=5
+#SBATCH --ntasks=10
 #SBATCH --partition=fast
 
-
-module load mpich
 mkdir ./quay_divisions
 
-
-for ((i = 5; i>=5; i = i - 5))
+#-t(inst) -p(port)
+for ((i = 50; i >= 10; i = i - 10))
 do
-	for((j = 1; j <= 1; j++))
+	for((j = 1; j <= 3; j++))
 	do
 		mpiexec ./p3a -t $i -p $1
 		python get_inst_time.py + "\n"
-		#python file_system.py
 	done
 done
 
 rsync -av --progress ./quay_divisions/* ./quay_divisions/$1 --exclude ./quay_divisions/$1/*
-#delete directories inside quay_divisions/.
-for((i = 0; i <= 10; i = i + 1))
+#delete directories inside quay_divisions/.   dir_name = /^\d\w+$/
+for((i = 0; i < 10; i = i + 1))
 do
 	rm -r ./quay_divisions/$i*
 done
