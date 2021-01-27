0fa0dad61134f3937f73ac0e3c4a06e590b2e14a
Raffel222 <Raffel222@gmail.com>
2021-01-27 12:55:25 +0100

    Update transfer_output_files.sh

--- transfer_output_files.sh
+++ transfer_output_files.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#$1 - port abbreviation ("lh", "sh" , "si", etc.), $2 - proces number, $3 - inst_times(s)
+#$1 - port abbreviation ("lh", "sh" , "si", etc.), $2 - number of processes, $3 - inst_times(s), $4 - std_output
 declare -i proc=$2
 
-#subfolder for proc number of processes
+#subfolder for number of processes
 mkdir -p ./quay_divisions/$1/p${proc}
-#rsync -av --progress ./quay_divisions/* ./quay_divisions/$1/p${proc} --exclude ./quay_divisions/$1
 mv ./quay_divisions/?_* ./quay_divisions/$1/p${proc}
 
 #move logs and execution times to process folder
 mv evaluator.log ./quay_divisions/$1/p${proc}
 mv local_search.log ./quay_divisions/$1/p${proc}
 mv $3 ./quay_divisions/$1/p${proc}
-
+mv $4 ./quay_divisions/$1/p${proc}
