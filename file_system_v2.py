c0086cccd375821c4419a8e4b916dcbf1ce1169e
Raffel222 <Raffel222@gmail.com>
2021-01-27 12:51:29 +0100

    Update file_system_v2.py
    redundant lines of code removed

--- file_system_v2.py
+++ file_system_v2.py
@@ -11,22 +11,22 @@ def get_bap_dir_name(bap_entry):
 
 fs_log = open("file_system.log", "w")
 port_name = sys.argv[1]
-max_proc_exec = sys.argv[2]
+proc_exec = sys.argv[2]
+port_path = "quay_divisions/" + port_name + "/" + "p" + str(proc_exec) + "/"
 
 with open('./p3a_config.json') as config_json:
     data = json.load(config_json)
 
 bap_algorithms = data["bap_algorithms"]
 bap_length = len(bap_algorithms)
-
 berths = data["berths"]
 
-evaluator_location = "quay_divisions/" + port_name + "/p" + str(max_proc_exec) + "/evaluator.log"
+evaluator_location = port_path + "evaluator.log"
 ev_log = open(evaluator_location, "r")
 Lines = ev_log.readlines()
 ev_log.close()
 
-berth_lengths = open("quay_divisions/" + port_name + "/p" + str(max_proc_exec) + "/berth_lengths.txt", "w")
+berth_lengths = open(port_path + "berth_lengths.txt", "w")
 berth_str = "berth lengths:\n["
 for berth in berths:
 	berth_str = berth_str + str(berth) + ", "
@@ -40,18 +40,12 @@ berth_lengths.close()
 
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 bap_no = -1
-inst_no = 0
-BLF_dir = ""
 
 for line in Lines:
     if line[0] in numbers:
         bap_no = bap_no + 1
-        info_tmp = str(inst_no) + " " + str(bap_no) + " " + line
         # file path - START
         original_umask = os.umask(0)
-	#port_path = "quay_divisions/" + port_name
-	port_path = "quay_divisions/" + port_name + "/" + "p" + str(max_proc_exec) + "/"
-        #BLF_path = port_path + "/p" + str(max_proc_exec) + BLF_dir
 	BLF_path = port_path + BLF_dir
         bap_dir = "/inst" + str(inst_no) + "/"
         whole_path = BLF_path + bap_dir
@@ -63,12 +57,11 @@ for line in Lines:
 	bap_file_name = get_bap_dir_name(bap_algorithms[bap_no]) + ".txt"
         bap_filepath = os.path.join(whole_path, bap_file_name)
         
-        
-
 	bap_lb_path = os.path.join(whole_path, "lb") + ".txt"
         bap_lb_file = open(bap_lb_path, "w")
 	bap_lb_file.write("Lower Bound: " + line.split()[2])
-        
+	bap_lb_file.close()
+
 	bap_file_w_mode = open(bap_filepath, "w")
 	bap_file_w_mode.write("MWFT(norm) MWFT LB\n")
         bap_file_w_mode.write(line)
